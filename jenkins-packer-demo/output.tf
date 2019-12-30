32d0b12658bef4848369ed598f7d150d4fb94820
raneshreyas86 <raneshreyas86@gmail.com>
2019-12-29 18:29:28 -0600

    Update output.tf

--- jenkins-packer-demo/output.tf
+++ jenkins-packer-demo/output.tf
@@ -1,6 +1,8 @@
 output "jenkins-ip" {
-  value = "${aws_instance.jenkins-instance.public_ip}"
+  value = aws_instance.jenkins-instance.public_ip
 }
 output "app-ip" {
-  value = "${aws_instance.app-instance.public_ip}"
+  # value = aws_instance.app-instance.count.public_ip
+  value = length(aws_instance.app-instance)
+  # value = aws_instance.app-instance[length(aws_instance.app-instance)].public_ip
 }
