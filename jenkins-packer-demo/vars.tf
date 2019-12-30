723ef3ebaece34501f4d770a3f981864f140709d
raneshreyas86 <raneshreyas86@gmail.com>
2019-12-29 18:31:01 -0600

    Update vars.tf

--- jenkins-packer-demo/vars.tf
+++ jenkins-packer-demo/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
@@ -8,18 +8,20 @@ variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
 variable "AMIS" {
-  type = "map"
+  type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    # Ubuntu AMI
+    #us-east-1 = "ami-000b3a073fc20e415"
+    us-east-1 = "ami-04b9e92b5572fa0d1" // Ubuntu Server 18.04 LTS (HVM), SSD Volume Type
+    # Amazon Linux AMI
+    # us-east-1 = "ami-00eb20669e0990cb4"
   }
 }
 variable "INSTANCE_DEVICE_NAME" {
-  default = "/dev/xvdh"
+  default = "/dev/xvdb"
 }
 variable "JENKINS_VERSION" {
-  default = "2.32.1"
+  default = "2.204.1"
 }
 
 variable "APP_INSTANCE_COUNT" {
